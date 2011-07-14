cd5be0ebdfe6960f64bc2bc997a98cd095df2a32
Harry Marr <harry.marr@gmail.com>
2011-07-14 15:11:43 -0700

    tidy

--- lib/grapi/resource.rb
+++ lib/grapi/resource.rb
@@ -6,72 +6,74 @@ module Grapi
       @client = client
     end
 
-    def self.from_hash(client, hash)
-      obj = self.new(client)
-      hash.each { |key,val| obj.send("#{key}=", val) }
-      obj
-    end
+    class << self
+      def from_hash(client, hash)
+        obj = self.new(client)
+        hash.each { |key,val| obj.send("#{key}=", val) }
+        obj
+      end
 
-    def self.find(client, id)
-      path = self::ENDPOINT.gsub(':id', id.to_s)
-      data = client.api_get(path)
-      self.from_hash(client, data)
-    end
+      def find(client, id)
+        path = self::ENDPOINT.gsub(':id', id.to_s)
+        data = client.api_get(path)
+        self.from_hash(client, data)
+      end
 
-    def self.date_writer(*args)
-      args.each do |attr|
-        define_method("#{attr.to_s}=".to_sym) do |date|
-          date = date.is_a?(String) ? DateTime.parse(date) : date
-          instance_variable_set("@#{attr}", date)
+      def date_writer(*args)
+        args.each do |attr|
+          define_method("#{attr.to_s}=".to_sym) do |date|
+            date = date.is_a?(String) ? DateTime.parse(date) : date
+            instance_variable_set("@#{attr}", date)
+          end
         end
       end
-    end
 
-    def self.date_accessor(*args)
-      attr_reader *args
-      date_writer *args
-    end
-
-    def self.reference_reader(*args)
-      attr_reader *args
-
-      args.each do |attr|
-        if !attr.to_s.end_with?('_id')
-          raise ArgumentError, 'reference_reader args must end with _id'
-        end
-
-        name = attr.to_s.sub(/_id$/, '')
-        define_method(name.to_sym) do
-          obj_id = instance_variable_get("@#{attr}")
-          klass = Grapi.const_get(name.camelize)
-          klass.find(@client, obj_id)
-        end
+      def date_accessor(*args)
+        attr_reader *args
+        date_writer *args
       end
-    end
 
-    def self.reference_writer(*args)
-      attr_writer *args
+      def reference_reader(*args)
+        attr_reader *args
 
-      args.each do |attr|
-        if !attr.to_s.end_with?('_id')
-          raise ArgumentError, 'reference_writer args must end with _id'
-        end
-
-        name = attr.to_s.sub(/_id$/, '')
-        define_method("#{name}=".to_sym) do |obj|
-          klass = Grapi.const_get(name.camelize)
-          if !obj.is_a?(klass)
-            raise ArgumentError, "Object must be an instance of #{klass}"
+        args.each do |attr|
+          if !attr.to_s.end_with?('_id')
+            raise ArgumentError, 'reference_reader args must end with _id'
           end
 
-          instance_variable_set("@#{attr}", obj.id)
+          name = attr.to_s.sub(/_id$/, '')
+          define_method(name.to_sym) do
+            obj_id = instance_variable_get("@#{attr}")
+            klass = Grapi.const_get(name.camelize)
+            klass.find(@client, obj_id)
+          end
         end
       end
-    end
 
-    def self.reference_accessor(*args)
-      reference_reader *args
-      reference_writer *args
+      def reference_writer(*args)
+        attr_writer *args
+
+        args.each do |attr|
+          if !attr.to_s.end_with?('_id')
+            raise ArgumentError, 'reference_writer args must end with _id'
+          end
+
+          name = attr.to_s.sub(/_id$/, '')
+          define_method("#{name}=".to_sym) do |obj|
+            klass = Grapi.const_get(name.camelize)
+            if !obj.is_a?(klass)
+              raise ArgumentError, "Object must be an instance of #{klass}"
+            end
+
+            instance_variable_set("@#{attr}", obj.id)
+          end
+        end
+      end
+
+      def reference_accessor(*args)
+        reference_reader *args
+        reference_writer *args
+      end
     end
 
     attr_accessor :id, :uri
