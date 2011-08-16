c2fb9b8c3adecc141d6779fb854f03cb4fd830f7
Harry Marr <harry.marr@gmail.com>
2011-08-15 23:15:24 -0700

    Removed redundant :sub_resource_uri attributes

--- lib/gocardless/pre_authorization.rb
+++ lib/gocardless/pre_authorization.rb
@@ -3,7 +3,7 @@ module GoCardless
     self.endpoint = '/pre_authorizations/:id'
 
     attr_accessor :max_amount, :currency, :amount, :frequency_length,
-                  :frequency_unit, :description, :sub_resource_uris
+                  :frequency_unit, :description
 
     reference_accessor :merchant_id, :user_id
     date_accessor :expires_at, :created_at
--- lib/gocardless/subscription.rb
+++ lib/gocardless/subscription.rb
@@ -4,8 +4,7 @@ module GoCardless
     self.endpoint = '/subscriptions/:id'
 
     attr_accessor  :amount, :currency, :frequency_length, :frequency_unit,
-                   :description, :setup_fee, :trial_length, :trial_unit,
-                   :sub_resource_uris
+                   :description, :setup_fee, :trial_length, :trial_unit
 
     reference_accessor :merchant_id, :user_id
 
