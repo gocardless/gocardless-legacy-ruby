ba54c08603b030e73c4d1dbfef7ee87a986cb412
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2012-08-27 10:02:32 +0200

    Add cancel! method to pre_authorization

--- lib/gocardless/pre_authorization.rb
+++ lib/gocardless/pre_authorization.rb
@@ -25,6 +25,12 @@ module GoCardless
     def create_bill(attrs)
       Bill.new_with_client(client, attrs.merge(:source => self)).save
     end
+
+    def cancel!
+      path = self.class.endpoint.gsub(':id', id.to_s) + '/cancel'
+      client.api_put(path)
+    end
+
   end
 end
 
